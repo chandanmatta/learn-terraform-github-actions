6fb84712a3ee3f1cc1ef98a494e71d55d84c2b19
Chandan Matta <chandan.matta@cbrands.com>
2023-06-20 11:13:29 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -2,29 +2,23 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  cloud {
+    organization = "terraform-cloud-organization-chandan"
+    workspaces {
+      name = "demo-github-actions"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.52.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
   }
   required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
